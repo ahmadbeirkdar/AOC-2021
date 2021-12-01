8f2a3380f25614039f194031c85f9796dd3c92b8
Ahmad Beirkdar <ahmadb@protonmail.com>
2021-12-01 01:39:08 -0500

    better rs Thanks @LesleyLai6

--- day01/main.rs
+++ day01/main.rs
@@ -2,6 +2,18 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+// Thanks @LesleyLai6
+fn better_solve1(v: Vec<i32>) -> usize {
+    return v.windows(2)
+            .filter(|x| (x[1] - x[0]) > 0)
+            .count();
+}
+
+fn better_solve2(v: Vec<i32>) -> usize {
+    return v.windows(4)
+            .filter(|x| (x[3] - x[0]) > 0)
+            .count();
+}
 
 fn solve1(v: Vec<i32>) -> usize {
     return v.iter()
@@ -29,9 +41,9 @@ fn main() {
             }
         }
     }
-    
-    println!("Ans1: {}", solve1(input.clone()));
-    println!("Ans2: {}", solve2(input.clone()));
+
+    println!("Ans1: {}", better_solve1(input.clone()));
+    println!("Ans2: {}", better_solve2(input.clone()));
 }
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
